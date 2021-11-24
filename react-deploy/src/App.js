7e93b9448effaba1b3cf6d1d3f42d085356b8d36
Muey <habilitzz@hotmail.com>
2021-11-24 13:21:34 +0700

    edit app

--- react-deploy/src/App.js
+++ react-deploy/src/App.js
@@ -5,18 +5,18 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+        <h1>This is Tina</h1>
+        <iframe 
+        src="https://www.facebook.com/plugins/post.php?href=https%3A%2F%2Fweb.facebook.com%2Fhabilit%2Fposts%2F10158286020190817&show_text=true&width=500" 
+        width="500" 
+        height="466" 
+        className={'no-border'}
+        // style="border:none;overflow:hidden" 
+        scrolling="no" 
+        frameborder="0" 
+        allowfullscreen="true" 
+        allow="autoplay; clipboard-write; encrypted-media; picture-in-picture; web-share">
+        </iframe>
       </header>
     </div>
   );
